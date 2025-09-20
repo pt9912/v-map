0728c0928010f52eb7864c9eaa5cfbeaec52253c
pt9912 <dburkard@pm.me>
2025-09-20 18:18:03 +0000

    update storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,25 @@
 const config = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   staticDirs: ['../dist/esm-es5'],
-  addons: ['@storybook/addon-links', '@storybook/addon-docs'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-docs',
+    //'@storybook/addon-controls',
+  ],
   core: {
     disableTelemetry: true,
   },
   framework: {
     name: '@stencil/storybook-plugin',
   },
+  // // Wichtig: Webpack-Konfiguration für .md-Dateien
+  // webpackFinal: async config => {
+  //   config.module.rules.push({
+  //     test: /\.md$/,
+  //     use: 'raw-loader',
+  //   });
+  //   return config;
+  // },
 };
 
 export default config;
